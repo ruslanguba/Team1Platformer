d366acf049fb823a9c1c881ced5c020543967090
ruslanguba <122637366+ruslanguba@users.noreply.github.com>
2025-03-30 16:44:47 +0200

    UIPrefDel

--- Assets/ProjectFolder/Ruslan/Scripts/Markers/CompleatLevel.cs
+++ Assets/ProjectFolder/Ruslan/Scripts/Markers/CompleatLevel.cs
@@ -5,24 +5,12 @@ public class CompleatLevel : MonoBehaviour
     [SerializeField] GameObject _scorePanel;
     private bool _isMovingToFinish;
     private CharacterMovementHandler _moveable;
-    private RespawnHandler _spawnable;
 
-    private void Awake()
-    {
-        _spawnable = FindFirstObjectByType<RespawnHandler>();
-    }
-    private void OnEnable()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.TryGetComponent(out CharacterMoveController controller))
         {
-            controller.enabled = false;
-            _isMovingToFinish = true;
-            _moveable = controller.gameObject.GetComponent<CharacterMovementHandler>();
-            _scorePanel.SetActive(true);
+            TakeControl(controller);
         }
     }
 
@@ -37,5 +25,13 @@ public class CompleatLevel : MonoBehaviour
         {
             _moveable.Move(Vector2.right);
         }
+    }
+
+    private void TakeControl(CharacterMoveController controller)
+    {
+        controller.enabled = false;
+        _isMovingToFinish = true;
+        _moveable = controller.gameObject.GetComponent<CharacterMovementHandler>();
+        _scorePanel.SetActive(true);
     }
 }
